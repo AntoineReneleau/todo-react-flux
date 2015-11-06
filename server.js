b8b757cc455e6e61a6ed793d01ae01f42854efa8
Mathieu Molimard <mathieu@zol.fr>
2015-11-06 11:59:28 +0100

    docker

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+
+var app = express();
+
+app.use(serveStatic('/src', {'index': ['index.htm']}));
+//app.listen(80)
+
+var server = app.listen(80, '0.0.0.0', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Demo server listening at http://%s:%s', host, port);
+});
